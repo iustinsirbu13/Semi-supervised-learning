82ef62de42954165d72ff851dcb942d1f6f1377a
robert.popovici <robert.popovici@fep8.grid.pub.ro>
2024-06-23 13:24:51 +0300

    Fix cluster demo

--- run-batch.sh
+++ run-batch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sbatch -p ucsx --gres gpu:1 --time=360 --cpus-per-task=1 --mem=32G ./apptainer-exec.sh $1
+sbatch -p ucsx --gres gpu:1 --time=360 --cpus-per-task=5 --mem-per-cpu=16G ./apptainer-exec.sh $1
 
